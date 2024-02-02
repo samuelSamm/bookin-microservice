f72a44b662937617492df7f60806e8d38ad58fab
SamuelSam <60914222+samuelSamm@users.noreply.github.com>
2024-02-02 17:09:49 -0600

    backup my code

---
+++ src/main/java/com/jafra/bookinmicroservice/BookinMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.jafra.bookinmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookinMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookinMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jafra/bookinmicroservice/BookinMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jafra.bookinmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookinMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
